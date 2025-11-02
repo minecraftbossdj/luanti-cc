d20cfa77eb9ec9693c7df26266c2f6e3bcabdb60
Gavin S <gavins@obsidiancorestudios.com>
2025-11-01 22:29:10 -0400

    feat: implemented basic file helper functions for checking if files exist, added computercraft folder generation

--- init.lua
+++ init.lua
@@ -1,5 +1,43 @@
 MODID = "cc_reimagined"
 
-local modpath = minetest.get_modpath("cc_reimagined")
+local modpath = core.get_modpath("cc_reimagined")
+local worldpath = core.get_worldpath()
 
 dofile(modpath.."/blocks/computer_normal.lua")
+
+HELPER =  {}
+
+function HELPER.fileExists(file_path)
+    local file = io.open(worldpath..file_path,"r")
+    if file then
+        file:close()
+        return true
+    else
+        return false
+    end
+end
+
+function HELPER.dirExists(directory_path)
+    local file = io.open(worldpath..directory_path.."/temp.txt","w")
+    if file then
+        file:close()
+        os.remove(worldpath..directory_path.."/temp.txt")
+        return true
+    else
+        return false
+    end
+end
+
+if not HELPER.dirExists("/computercraft") then
+    core.log("Computercraft directory does not exist!")
+    core.mkdir(worldpath.."/computercraft")
+    local computer_ids_file = io.open(worldpath.."/computercraft/computers.json","w")
+    local data = {
+        next_id = 0
+    }
+    local data_json = core.write_json(data)
+    computer_ids_file:write(data_json)
+    computer_ids_file:close()
+else
+    core.log("Computercraft directory exists!")
+end
