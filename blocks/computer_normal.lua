d2985414c5be404c81b4e84bde5b2752cebd41b7
Gavin S <gavins@obsidiancorestudios.com>
2025-11-01 21:09:04 -0400

    feat: setup basic systems of the mod

---
+++ blocks/computer_normal.lua
@@ -0,0 +1,12 @@
+core.register_node(MODID..":computer_normal", {
+    description = "Computer",
+    tiles = {
+        "computer_normal_top.png",
+        "computer_normal_bottom.png",
+        "computer_normal_side.png",
+        "computer_normal_side.png",
+        "computer_normal_side.png",
+        "computer_normal_front.png",
+    },
+    groups = {oddly_breakable_by_hand = 2},
+})
---
+++ init.lua
@@ -0,0 +1,5 @@
+MODID = "cc_reimagined"
+
+local modpath = minetest.get_modpath("cc_reimagined")
+
+dofile(modpath.."/blocks/computer_normal.lua")
