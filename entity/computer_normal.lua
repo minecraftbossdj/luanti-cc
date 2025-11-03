44d397fe70e2be93f1fc163a63a9bd2ffa657a0b
riley <minecraftbossdj@gmail.com>
2025-11-03 14:55:20 -0500

    renamed minetest to core, since "minetest" only exists for backwards compatibility

--- entity/computer_normal.lua
+++ entity/computer_normal.lua
@@ -7,7 +7,7 @@ core.register_on_player_receive_fields(function(player, formname, fields)
     local function isForm(name)
         return string.sub(formname,1,string.len(name))==name
     end
-    --minetest.debug("FORM SUBMITTED",dump(player),dump(formname),dump(fields))
+    --core.debug("FORM SUBMITTED",dump(player),dump(formname),dump(fields))
     if isForm(FORMNAME_TURTLE_INVENTORY) then
         local id = tonumber(string.sub(formname,1+string.len(FORMNAME_TURTLE_INVENTORY)))
         if fields.upload_code=="Upload Code" then
@@ -30,12 +30,12 @@ core.register_on_player_receive_fields(function(player, formname, fields)
         command = "function init(turtle) return "..command.." end"
         local commandResult = turtle:upload_code_to_turtle(player,command, true)
         if commandResult == nil then
-            minetest.close_formspec(player:get_player_name(),FORMNAME_TURTLE_TERMINAL..id)
+            core.close_formspec(player:get_player_name(),FORMNAME_TURTLE_TERMINAL..id)
             return true
         end
         commandResult = fields.terminal_in.." -> "..commandResult
         turtle.previous_answers[#turtle.previous_answers+1] = commandResult
-        minetest.show_formspec(player:get_player_name(),FORMNAME_TURTLE_TERMINAL..id,turtle:get_formspec_terminal());
+        core.show_formspec(player:get_player_name(),FORMNAME_TURTLE_TERMINAL..id,turtle:get_formspec_terminal());
     elseif isForm(FORMNAME_TURTLE_UPLOAD) then
         local id = tonumber(string.sub(formname,1+string.len(FORMNAME_TURTLE_UPLOAD)))
         if fields.button_upload == nil or fields.upload == nil then
